ad37acc0e921985a000bdd2832e3fc6de59d71d7
Simon Strandgaard <neoneye@gmail.com>
2013-12-13 22:39:55 +0100

    Migrated to ARC - sc_finder_info_manager

--- source/SharedCode Core+Worker/source/sc_finder_info_manager.m
+++ source/SharedCode Core+Worker/source/sc_finder_info_manager.m
@@ -1,3 +1,7 @@
+#if ! __has_feature(objc_arc)
+#error This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
 #import "sc_finder_info_manager.h"
 #import <CoreServices/CoreServices.h>
 
