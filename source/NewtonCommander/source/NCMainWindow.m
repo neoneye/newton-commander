c47450e46e56e999108b461e7135b7c74e22590a
Simon Strandgaard <neoneye@gmail.com>
2013-12-17 23:33:46 +0100

    Migrated to ARC.

--- source/NewtonCommander/source/NCMainWindow.m
+++ source/NewtonCommander/source/NCMainWindow.m
@@ -5,6 +5,10 @@
 //  Created by Simon Strandgaard on 21/06/10.
 //  Copyright 2010 opcoders.com. All rights reserved.
 //
+#if ! __has_feature(objc_arc)
+#error This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
 
 #import "NCMainWindow.h"
 
