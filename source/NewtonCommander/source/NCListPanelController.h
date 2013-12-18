25bdbd7c667ee8443ea1709c97caf1c440988401
Simon Strandgaard <neoneye@gmail.com>
2013-12-18 16:48:20 +0100

    Extracted NCPanelControllerDelegate from the NCListPanelController. It is used other places too.

--- source/NewtonCommander/source/NCListPanelController.h
+++ source/NewtonCommander/source/NCListPanelController.h
@@ -7,22 +7,11 @@
 //
 
 #import <Cocoa/Cocoa.h>
-
+#import "NCPanelControllerDelegate.h"
 
 @class NCTabArray;
 @protocol NCCopyOperationProtocol;
 @protocol NCMoveOperationProtocol;
-
-@protocol NCPanelControllerDelegate <NSObject>
-@optional
--(void)tabKeyPressed:(id)sender;
--(void)switchToNextTab:(id)sender;
--(void)switchToPrevTab:(id)sender;
--(void)closeTab:(id)sender;
--(void)clickToActivatePanel:(id)sender;
--(void)workingDirDidChange:(id)sender;
--(void)tabViewItemsDidChange:(id)sender;
-@end
 
 @interface NCListPanelController : NSViewController
 
---
+++ source/NewtonCommander/source/NCPanelControllerDelegate.h
@@ -0,0 +1,22 @@
+//
+//  NCPanelControllerDelegate.h
+//  NewtonCommander
+//
+//  Created by Simon Strandgaard on 12/18/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NCPanelControllerDelegate <NSObject>
+
+@optional
+-(void)tabKeyPressed:(id)sender;
+-(void)switchToNextTab:(id)sender;
+-(void)switchToPrevTab:(id)sender;
+-(void)closeTab:(id)sender;
+-(void)clickToActivatePanel:(id)sender;
+-(void)workingDirDidChange:(id)sender;
+-(void)tabViewItemsDidChange:(id)sender;
+
+@end
